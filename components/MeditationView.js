de0dfe675cf96977d1caaf4ce6c2bb0600e0ab8a
Leon Kuhne <x@mail.rit.edu>
2019-01-05 00:23:36 -0500

    added button for meditation

--- components/MeditationView.js
+++ components/MeditationView.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Button } from 'react-native'
+import Colors from '../constants/Colors'
 import pi from '../api/pi'
 
 const styles = StyleSheet.create({
@@ -14,6 +15,7 @@ export default class MeditationView extends React.Component {
     return(
       <View style={styles.container}>
         <Text>{this.props.title}</Text>
+        <Button onPress{()=>this.addMeditation()} title="Meditate" color={Colors.button} />
       </View>
     )
   }

43fe983264ea2d687bc2a1d6cb4828d329adb235
Leon Kuhne <x@mail.rit.edu>
2019-01-05 11:21:42 -0500

    added logging output

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 git pull
-expo build:android
+expo build:android > log.txt
+# grep the file for the url after 'Successfully built standalone app: '
+# push url to leonkuhne.com/apk
 read  -n 1 -p "Press any key to end..." key

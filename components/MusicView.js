4a298601de64b265d26973ed085e7b73a1eb76f6
Leon Kuhne <x@mail.rit.edu>
2019-01-05 15:59:41 -0500

    fixed music stuff

--- components/MusicView.js
+++ components/MusicView.js
@@ -3,6 +3,8 @@ import { StyleSheet, View, Text, Button } from 'react-native'
 import Colors from '../constants/Colors'
 import pi from '../api/pi'
 
+const GOAL_HOURS = 500
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -20,14 +22,45 @@ const styles = StyleSheet.create({
 export default class MusicView extends React.Component {
   
   render() {
+    let onCourse = this.props.diff >= 0
+    let ratioStr = `${this.props.hours}/${GOAL_HOURS}`
+
+    let diffStr
+    let bgColor
+    if(onCourse) {
+      diffStr = '+' + this.props.diff
+      bgColor = Colors.bg
+    } else {
+      diffStr = '' + this.props.diff
+      bgColor = Colors.warningBackground
+    }
+
+    let title = `Music ${ratioStr} ${diffStr}`
+
     return(
-      <View style={styles.container}>
-        <Text>{this.props.title}</Text>
+      <View style={[styles.container, {backgroundColor: bgColor}]}>
+        <Text>{title}</Text>
         <View style={styles.buttonContainer}>
-          <Button onPress={()=>this.addMusic(15,'min')} title="15 Min" color={Colors.button} />
-          <Button onPress={()=>this.addMusic(30,'min')} title="30 Min" color={Colors.button} />
-          <Button onPress={()=>this.addMusic(1,'hrs')} title="1 Hour" color={Colors.button} />
-          <Button onPress={()=>this.addMusic(2,'hrs')} title="2 Hours" color={Colors.button} />
+          <Button
+            onPress={()=>this.addMusic(15,'min')}
+            title="15 Min"
+            color={Colors.button}
+          />
+          <Button
+            onPress={()=>this.addMusic(30,'min')}
+            title="30 Min"
+            color={Colors.button}
+          />
+          <Button
+            onPress={()=>this.addMusic(1,'hrs')}
+            title="1 Hour"
+            color={Colors.button}
+          />
+          <Button
+            onPress={()=>this.addMusic(2,'hrs')}
+            title="2 Hours"
+            color={Colors.button}
+          />
         </View>
       </View>
     )

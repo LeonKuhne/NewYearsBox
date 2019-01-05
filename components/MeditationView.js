4ca528fcf68534d2c5e2c80b9d61c56c8161373f
Leon Kuhne <x@mail.rit.edu>
2019-01-04 23:05:57 -0500

    fixed import

--- components/MeditationView.js
+++ components/MeditationView.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, Text }
+import { StyleSheet, View, Text } from 'react-native'
 import pi from '../api/pi'
 
 const styles = StyleSheet.create({
--- components/MusicView.js
+++ components/MusicView.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, Text, Button }
+import { StyleSheet, View, Text, Button } from 'react-native'
 import pi from '../api/pi'
 
 const styles = StyleSheet.create({

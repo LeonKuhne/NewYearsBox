79f2eff3a68990a4b465d161e8acc87b7a103d03
Leon Kuhne <x@mail.rit.edu>
2019-01-05 13:40:34 -0500

    added autostarting py on deploy

--- start.sh
+++ start.sh
@@ -5,4 +5,5 @@ prefix="Successfully built standalone app:.*"
 fullUrl=$(cat log.txt | grep -o "Successfully built standalone app: .*")
 url=$(echo "${fullUrl}" | sed -e 's/^[ \t]*Successfully built standalone app: //')
 echo "function openUrl(){window.open('$url', '_blank')}" | ssh root@104.207.135.80 "cat > /var/www/html/apk/script.js"
-read  -n 1 -p "Press any key to end..." key
+sudo ssh -t pi@192.168.1.3 "cd ~/Desktop/NewYearsBox && python start.py ; bash"
+read -n 1 -p "Press any key to end..." key

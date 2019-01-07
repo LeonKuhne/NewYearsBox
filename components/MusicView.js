2125056a1b217599d4c48e82de6b3e63a2009b03
Leon Kuhne <x@mail.rit.edu>
2019-01-07 07:35:08 -0500

    added import for buttongroup

--- components/MusicView.js
+++ components/MusicView.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, Text, Button } from 'react-native'
+import { StyleSheet, View, Text, ButtonGroup } from 'react-native'
 import Colors from '../constants/Colors'
 import pi from '../api/pi'
 
